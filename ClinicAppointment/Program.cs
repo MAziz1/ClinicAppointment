4cd6c9768104d9ffe912659a3efa05520cd26608
moham <mabdelarzek@alasilacx.sa>
2022-08-09 00:56:33 +0200

    add docker compose

--- ClinicAppointment/Program.cs
+++ ClinicAppointment/Program.cs
@@ -1,11 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
+using ClinicAppointment.Domain.Interfaces;
+using ClinicAppointment.Domain.Services;
+using ClinicAppointment.Infrastructure;
+using ClinicAppointment.Infrastructure.NPGSRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add DbContext
+builder.Services.AddDbContext<AppointmentDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("Default")));
+
+// Services
+builder.Services.AddScoped(typeof(IRepository<>), typeof(NPGSRepository<>));
+builder.Services.AddScoped<IPatientService, PatientService>();
+builder.Services.AddScoped<IAppointmentService, AppointmentService>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
 
 var app = builder.Build();
 
